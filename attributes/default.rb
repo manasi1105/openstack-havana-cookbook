d243e3af64815768817dcfa916a4fbfe9a6deaaf
manasi1105 <manasi.prabhavalkar11@gmail.com>
2014-05-06 11:44:45 -0400

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -15,7 +15,7 @@ largest_vg  = vg.stdout[0..-2]
 
 default[:creds][:admin_password]  = "ADMIN_PASS"
 default[:creds][:mysql_password]  = "SQL_DBPASS"
-default[:creds][:keystone_token]  = "KEYSTONE_DBPASS"
+default[:creds][:keystone_token]  = "SQL_DBPASS"
 default[:creds][:swift_hash]      = SecureRandom.urlsafe_base64(20)
 default[:creds][:neutron_secret]  = "NEUTRON_DBPASS"
 default[:creds][:ssh_keypair]     = "openstack"
